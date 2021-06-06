a434aa289c50eef20640be47b51890d00c83b373
idanah7 <idanah@post.bgu.ac.il>
2021-06-07 01:58:46 +0300

    gssdsg

--- src/BL/ConsoleColors.java
+++ src/BL/ConsoleColors.java
@@ -74,8 +74,4 @@ public class ConsoleColors {
     public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
     public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
 
-    public static void main(String[] args) {
-        System.out.println(ConsoleColors.RED_BACKGROUND_BRIGHT + "RED COLORED" +
-                ConsoleColors.RESET + " NORMAL");
-    }
 }
